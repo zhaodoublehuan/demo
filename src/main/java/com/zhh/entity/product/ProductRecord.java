595188857d475741d439e77de96ae2a2bd4fa493
zhaodoublehuan <zhao,.117>
2018-08-24 14:02:38 +0800

    代码优化

--- src/main/java/com/zhh/entity/product/ProductRecord.java
+++ src/main/java/com/zhh/entity/product/ProductRecord.java
@@ -24,8 +24,8 @@ public class ProductRecord {
     //进出货表示
     private DictData productInoutType;
     //进出货时间
-    @JsonFormat(pattern="yyyy-MM-dd",timezone = "GMT+8")
-    @DateTimeFormat(pattern="yyyy-MM-dd")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date inoutDate;
     //新增时间
     private Date insertDate;
